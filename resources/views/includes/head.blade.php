fd038305fce4668f95901369249ded6971b7861d
Ali <ali.haider@worthdoux.com>
2019-06-13 22:34:30 +0500

    first commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from xvelopers.com/demos/html/paper-panel/panel-page-blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jun 2019 14:44:45 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/img/basic/favicon.ico" type="image/x-icon">
+    <title>Paper</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/app.css">
+    <style>
+        .loader {
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #F5F8FA;
+            z-index: 9998;
+            text-align: center;
+        }
+
+        .plane-container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+        }
+    </style>
+    <!-- Js -->
+    <!--
+    --- Head Part - Use Jquery anywhere at page.
+    --- http://writing.colin-gourlay.com/safely-using-ready-before-including-jquery/
+    -->
+    <script>(function(w,d,u){w.readyQ=[];w.bindReadyQ=[];function p(x,y){if(x=="ready"){w.bindReadyQ.push(y);}else{w.readyQ.push(x);}};var a={ready:p,bind:p};w.$=w.jQuery=function(f){if(f===d||f===u){return a}else{p(f)}}})(window,document)</script>
+</head>
---
+++ resources/views/includes/master.blade.php
@@ -0,0 +1,19 @@
+@include('includes.head')
+@include('includes.sidebar')
+@include('includes.header')
+
+
+
+@yield('content')
+
+<div class="control-sidebar-bg shadow white fixed"></div>
+</div>
+<!--/#app -->
+
+@include('includes.foot')
+
+
+</body>
+
+</html>
+
